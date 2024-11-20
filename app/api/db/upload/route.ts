cacfad9fd32d09449d78c5ab119355c1e8fb5b8f
DAlsabrook <112190470+DAlsabrook@users.noreply.github.com>
2024-11-20 15:52:57 -0600

    last push

--- app/api/db/upload/route.ts
+++ app/api/db/upload/route.ts
@@ -3,6 +3,7 @@ import { type NextRequest } from 'next/server'
 // POST route to upload csv file to be cleaned and uploaded to db via lib/mongoose.tsx
 export const POST = async (req: NextRequest, res:Response) => {
   const data = req.body;
+  // Create function in lib folder to handle cleaning and uploading file data
   return new Response(JSON.stringify({ message: 'API is ready and working' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
